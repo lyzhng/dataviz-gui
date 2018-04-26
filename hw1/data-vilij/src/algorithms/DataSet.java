15c7f7faaabede39e499cba9fb11e10ee713b604
lilyzhong <lily.zhong@stonybrook.edu>
2018-04-26 16:51:12 -0400

    hardcoding is removed (final draft)

---
+++ hw1/data-vilij/src/algorithms/DataSet.java
@@ -0,0 +1,102 @@
+package algorithms;
+import dataprocessors.TSDProcessor;
+import javafx.geometry.Point2D;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+
+/**
+ * This class specifies how an algorithm will expect the dataset to be. It is
+ * provided as a rudimentary structure only, and does not include many of the
+ * sanity checks and other requirements of the use cases. As such, you can
+ * completely write your own class to represent a set of data instances as long
+ * as the algorithm can read from and write into two {@link java.util.Map}
+ * objects representing the name-to-label map and the name-to-location (i.e.,
+ * the x,y values) map. These two are the {@link DataSet#labels} and
+ * {@link DataSet#locations} maps in this class.
+ *
+ * @author Ritwik Banerjee
+ */
+public class DataSet {
+
+    public static class InvalidDataNameException extends Exception {
+
+        private static final String NAME_ERROR_MSG = "All data instance names must start with the @ character.";
+
+        public InvalidDataNameException(String name) {
+            super(String.format("Invalid name '%s'." + NAME_ERROR_MSG, name));
+        }
+    }
+
+    private static String nameFormatCheck(String name) throws InvalidDataNameException {
+        if (!name.startsWith("@"))
+            throw new InvalidDataNameException(name);
+        return name;
+    }
+
+    private static Point2D locationOf(String locationString) {
+        String[] coordinateStrings = locationString.trim().split(",");
+        return new Point2D(Double.parseDouble(coordinateStrings[0]), Double.parseDouble(coordinateStrings[1]));
+    }
+
+    private Map<String, String>  labels;
+    private Map<String, Point2D> locations;
+    TSDProcessor processor;
+
+    /** Creates an empty dataset. */
+    public DataSet() {
+        labels = new HashMap<>();
+        locations = new HashMap<>();
+    }
+
+    public DataSet(TSDProcessor processor) {
+        labels = new HashMap<>();
+        locations = new HashMap<>();
+        this.processor = processor;
+    }
+
+    public Map<String, String> getLabels()     { return labels; }
+
+    public Map<String, Point2D> getLocations() { return locations; }
+
+    public void updateLabel(String instanceName, String newlabel) {
+        if (labels.get(instanceName) == null)
+            throw new NoSuchElementException();
+        labels.put(instanceName, newlabel);
+    }
+
+    private void addInstance(String tsdLine) throws InvalidDataNameException {
+        String[] arr = tsdLine.split("\t");
+        labels.put(nameFormatCheck(arr[0]), arr[1]);
+        locations.put(arr[0], locationOf(arr[2]));
+    }
+
+    public static DataSet fromTSDFile(Path tsdFilePath) throws IOException {
+        DataSet dataset = new DataSet();
+        Files.lines(tsdFilePath).forEach(line -> {
+            try {
+                dataset.addInstance(line);
+            } catch (InvalidDataNameException e) {
+                e.printStackTrace();
+            }
+        });
+        return dataset;
+    }
+
+    public static DataSet fromTSDProcessor(String tsdString) {
+        DataSet dataSet = new DataSet();
+        Stream.of(tsdString.split("\n")).forEach(line -> {
+            try {
+                dataSet.addInstance(line);
+            } catch (InvalidDataNameException e) {
+                e.printStackTrace();
+            }
+        });
+        return dataSet;
+    }
+}
---
+++ hw1/data-vilij/src/dialogs/ExitWhileUnfinishedDialog.java
@@ -0,0 +1,107 @@
+package dialogs;
+
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import vilij.components.ConfirmationDialog;
+import vilij.components.Dialog;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author The author of this document is Lily Zhong.
+ */
+public class ExitWhileUnfinishedDialog extends Stage implements Dialog
+{
+    /**
+     * This class provides the template for displaying three-way confirmation messages to the end user of a Vilij
+     * application. It always provides the two controls: (i) a customizable message, and (ii) a set of three buttons
+     * corresponding to the three standard options for the user. The three options (<code>Yes</code>, <code>No</code>, and
+     * <code>Cancel</code> are modeled as the inner enumerable class {@link vilij.components.ConfirmationDialog.Option}.
+     *
+     * @author Ritwik Banerjee
+     */
+        public enum Option {
+
+            RETURN("Return"),
+            EXIT("Exit");
+
+            @SuppressWarnings("unused")
+            private String option;
+
+            Option(String option) { this.option = option; }
+        }
+
+        private static ExitWhileUnfinishedDialog dialog;
+
+        private Label message = new Label();
+        private ExitWhileUnfinishedDialog.Option selectedOption;
+
+        private ExitWhileUnfinishedDialog() { /* empty constructor */ }
+
+        public static ExitWhileUnfinishedDialog getDialog() {
+            if (dialog == null)
+                dialog = new ExitWhileUnfinishedDialog();
+            return dialog;
+        }
+
+        private void setMessage(String msg) { message.setText(msg); }
+
+        private void deleteOptionHistory()                  { selectedOption = null; }
+
+        /**
+         * Completely initializes the error dialog to be used.
+         *
+         * @param owner the window on top of which the error dialog window will be displayed
+         */
+        @Override
+        public void init(Stage owner) {
+            initModality(Modality.WINDOW_MODAL); // modal => messages are blocked from reaching other windows
+            initOwner(owner);
+
+            List<Button> buttons = Arrays.asList(
+                    new Button(Option.RETURN.name()),
+                    new Button(Option.EXIT.name()));
+
+            buttons.forEach(button -> button.setOnAction((ActionEvent event) -> {
+                this.selectedOption = Option.valueOf(((Button) event.getSource()).getText());
+                this.hide();
+            }));
+
+            HBox buttonBox = new HBox(5);
+            buttonBox.getChildren().addAll(buttons);
+
+            VBox messagePane = new VBox(message, buttonBox);
+            messagePane.setAlignment(Pos.CENTER);
+            messagePane.setPadding(new Insets(10, 20, 20, 20));
+            messagePane.setSpacing(10);
+
+            this.setScene(new Scene(messagePane));
+        }
+
+        /**
+         * Loads the specified title and message into the error dialog and then displays the dialog.
+         *
+         * @param dialogTitle the specified dialog title
+         * @param message     the specified message
+         */
+        @Override
+        public void show(String dialogTitle, String message) {
+            deleteOptionHistory();           // delete any previously selected option
+            setTitle(dialogTitle);           // set the title of the dialog
+            setMessage(message); // set the main error message
+            showAndWait();                   // open the dialog and wait for the user to click the close button
+        }
+
+
+        public ExitWhileUnfinishedDialog.Option getSelectedOption() { return selectedOption; }
+    }
+
